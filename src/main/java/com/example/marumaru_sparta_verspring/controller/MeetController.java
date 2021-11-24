38d067d659962459b5de2a773f50ab9029018b83
thalals <thalsal@naver.com>
2021-11-24 15:54:57 +0900

    #3 게시글 리스트 및 생성 UI

--- src/main/java/com/example/marumaru_sparta_verspring/controller/MeetController.java
+++ src/main/java/com/example/marumaru_sparta_verspring/controller/MeetController.java
@@ -1,6 +1,5 @@
 package com.example.marumaru_sparta_verspring.controller;
 
-import com.example.marumaru_sparta_verspring.domain.S3Uploader;
 import com.example.marumaru_sparta_verspring.domain.posts.Meet;
 import com.example.marumaru_sparta_verspring.dto.MeetRequestDto;
 import com.example.marumaru_sparta_verspring.service.MeetService;
@@ -15,15 +14,15 @@ import java.io.IOException;
 public class MeetController {
 
     private final MeetService meetService;
-    private final S3Uploader s3Uploader;
+//    private final S3Uploader s3Uploader;
 
     @PostMapping("/meets")
     public Meet saveMeet(@RequestBody MeetRequestDto meetRequestDto) {
         return meetService.saveMeet(meetRequestDto);
     }
 
-    @PostMapping("/upload")
-    public String upload(@RequestParam("data") MultipartFile multipartFile) throws IOException {
-        return s3Uploader.upload(multipartFile, "static");
-    }
+//    @PostMapping("/upload")
+//    public String upload(@RequestParam("data") MultipartFile multipartFile) throws IOException {
+//        return s3Uploader.upload(multipartFile, "static");
+//    }
 }
---
+++ src/main/java/com/example/marumaru_sparta_verspring/controller/PostController.java
@@ -0,0 +1,13 @@
+package com.example.marumaru_sparta_verspring.controller;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    
+}
