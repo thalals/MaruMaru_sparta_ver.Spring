befb7d65a0c28595e34b30cecd7ddec1b3db58bd
sendkite <sendkite1@gmail.com>
2021-11-26 14:55:41 +0900

    ADD-aws S3, 주소api, datepicker

--- src/main/java/com/example/marumaru_sparta_verspring/controller/MeetController.java
+++ src/main/java/com/example/marumaru_sparta_verspring/controller/MeetController.java
@@ -1,16 +1,21 @@
 package com.example.marumaru_sparta_verspring.controller;
 
+import com.example.marumaru_sparta_verspring.domain.S3Uploader;
 import com.example.marumaru_sparta_verspring.domain.articles.Meet;
 import com.example.marumaru_sparta_verspring.dto.MeetRequestDto;
 import com.example.marumaru_sparta_verspring.service.MeetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
 public class MeetController {
 
     private final MeetService meetService;
+    private final S3Uploader s3Uploader;
 
     @PostMapping("/meets")
     public Meet saveMeet(@RequestBody MeetRequestDto meetRequestDto) {
@@ -18,4 +23,8 @@ public class MeetController {
     }
 
 
+    @PostMapping("/images")
+    public String upload(@RequestParam("images") MultipartFile multipartFile) throws IOException {
+        return s3Uploader.upload(multipartFile, "static");
+    }
 }
