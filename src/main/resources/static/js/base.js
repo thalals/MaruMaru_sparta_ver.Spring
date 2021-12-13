d47feedc2f57475188b43d84a84751ef655a2bb7
Jenny <imj1996@naver.com>
2021-12-13 16:44:53 +0900

    url 수정

--- src/main/resources/static/js/base.js
+++ src/main/resources/static/js/base.js
@@ -63,40 +63,5 @@ function get_card() {
 function logout() {
     localStorage.removeItem("token");
     localStorage.removeItem("username");
-    location.href = ebUrl;
+    location.href = "/";
 }
-
-// function addHTML(idx, dogImgUrl, dogName, dogAge, dogGender, dogComment) {
-//     //1. HTML 태그를 만든다.
-//     let tempHtml = `<div class="card color-card">
-//                         <ul>
-//                           <a href="#">
-//                           <li>
-//                           <i class="fas fa-heart" title="좋아요"></i>
-//                           </i>
-//                           </li>
-//                           </a>
-//                         </ul>
-//                         <div class="card_top">
-//                           <div>
-//                             <div class="profile_img">
-//                             <img class="dog_img" src="${dogImgUrl}">
-//                             </div>
-//                           </div>
-//                           <div>
-//                             <p class="name desc">${dogName}</p>
-//                           </div>
-//                           <div class="desc">
-//                             <p class="age">나이: ${dogAge}</p>
-//                             <p class="gender">성별: ${dogGender}</p>
-//                           </div>
-//                           <hr>
-//                           <div class="desc comment" style="font-size: 20px">
-//                             <p style="font-size: 15px">${dogComment}</p>
-//                             <button class="button color-a top mt-5" onclick = "location.href ='/profile/detail/${idx}'">자세히 보기</button>
-//                           </div>
-//                         </div>
-//                       </div>`;
-//     console.log(dogName, dogImgUrl)
-//     $("#profile_card_list").append(tempHtml);
-// }
