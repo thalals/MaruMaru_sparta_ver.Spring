f9e2e48da7881868caf4b0fdac7cac9a8aa84c78
thalals <thalsal@naver.com>
2022-03-27 20:12:49 +0900

    fix: Profile findAll fetch join user

--- src/main/java/com/example/marumaru_sparta_verspring/repository/ProfileRepository.java
+++ src/main/java/com/example/marumaru_sparta_verspring/repository/ProfileRepository.java
@@ -2,10 +2,14 @@ package com.example.marumaru_sparta_verspring.repository;
 
 import com.example.marumaru_sparta_verspring.domain.profile.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    @Override
+    @Query("SELECT p FROM Profile p join fetch p.user")
+    List<Profile> findAll();
     List<Profile> findByUserId(Long userId);
 } //Jpa 테이블 상속
