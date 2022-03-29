65839392ce95763d40c0fa359dcf162f260fc0ba
thalals <thalsal@naver.com>
2022-03-29 23:04:00 +0900

    fix: post 페이지네이션, Fetch join + batch size 100 으로 변경

--- src/main/java/com/example/marumaru_sparta_verspring/repository/PostRepository.java
+++ src/main/java/com/example/marumaru_sparta_verspring/repository/PostRepository.java
@@ -2,8 +2,12 @@ package com.example.marumaru_sparta_verspring.repository;
 
 import com.example.marumaru_sparta_verspring.domain.articles.Post;
 import com.example.marumaru_sparta_verspring.domain.user.User;
+import com.example.marumaru_sparta_verspring.dto.articles.PostResponseDto;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -12,4 +16,8 @@ public interface PostRepository extends JpaRepository<Post,Long> {
     List<Post> findAllByUser(User user);
     List<Post> findAllByTitleContainingIgnoreCase(String keyword);
     List<Post> findAllByContentContainingIgnoreCase(String keyword);
+//    @Query(value = "select * from post p join user u where p.user_id = u.id",nativeQuery = true)
+    @Query(value = "select p from Post p  join fetch p.user u", countQuery = "select count(p) from Post p")
+    Page<Post> findWithPagination(Pageable pageable);
+
 }
